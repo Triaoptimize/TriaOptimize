13c63ed8b20d7d7c697827ab48051a911c469eac
Vale_parra <131938990+cars200@users.noreply.github.com>
2024-09-20 15:03:12 -0500

    Se añade estructura para index.php

---
+++ Trimestre 3 /HTML y CSS/HTML/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tria Optimize</title>
+    <link rel="stylesheet" type="text/css" href="../css/index.css">
+    <link rel="shortcut icon" href="../img/logo.ico" type="image/x-icon">
+</head>
+
+<body>
+    <header class="Arriba">
+        <img src="../img/Logo.png" alt="">
+        <h1>Triü Raclette</h1>
+    </header>
+    <div class="nav ">
+        <nav class="navbar fixed-top">
+            <ul>
+                <li><a href="#nosotros"> Nosotros</a></li>
+                <li><a href="#menu">Menú</a></li>
+                <li><a href="#sede">Sedes</a></li>
+              <li>  <a class="botonn" href="ingreso.php">Soy empleado</a><i class="fi fi-ss-user"></i></li>
+            </ul>
+        </nav>
+     
+    </div>
+ 
+    <section class="Informacion" id="nosotros">
+        <div id="info">
+            <h1>Sobre nosotros</h1>
+            <P>En TRIU, celebramos la rica tradicion y la pasion
+                por la gastronimia que une las personas. Somos un restaurante
+                dedicado a ofrecer una experiencia culinaria única, especializada
+                en la seleccion de vinos excepcionales y quesos gourmet de todo el mundo.
+                Nuestra misión es deleitar a nuestros clientes con combinaciones
+                de sabores cuidadosamente curadas, creando momentos inolvidables
+                en un ambiente acogedor y elegante.
+            </P>
+        </div>
+        <div class="galeria">
+            <img src="../img/gale1.jpg" alt="" width="100px" weigth="100px">
+            <img src="../img/galw2.jpg" alt="" width="100px" weigth="100px">
+            <img src="../img/gale3.jpg" alt="" width="100px" weigth="100px">
+            <img src="../img/gale4.jpg" alt="" width="100px" weigth="100px">
+            <img src="../img/gale5.jpg" alt="" width="100px" weigth="100px">
+        </div>
+        <div class="mapa">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.721471184842!2d-74.06360398831995!3d4.
+        643691442127334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f1
+        3.1!3m3!1m2!1s0x8e3f9bff9dfa58c5%3A0xcbef973b2d55ed04!2sTri%C3%BC%20Raclette%20Cheese!5e0!3m2!1ses!2sco
+        !4v1723732230888!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+        </div>
+        <div class="direc" id="sede">
+            <h1>SEDE PRINCIPAL</h1>
+            <img src="../img/negocio.jpeg" alt="" height="300px" width="400px">
+            <p>Dirección: Cra. 6 #58-38, Chapinero, Bogotá, Cundinamarca
+                Teléfono: 322 2346473
+                Horario:
+            </p>
+
+        </div>
+    </section>
+    <div class="menus" id="menu">
+        <h2>Vinos</h2>
+        <img src="../img/vinos.jpg" alt="VInos" height="250px" width="250px">
+        <h2>
+            sándwich</h2>
+        <img src="../img/sadwich.jpg" alt="Sadwich" height="250px" width="250px">
+
+        <img src="../img/quesos.jpg" alt="Quesos" height="250px" width="250px">
+        <h2>Quesos</h2>
+        <img src="../img/gour.jpg" alt="GOurmet" height="250px" width="250px">
+        <h2>Gourmet</h2>
+    </div>
+    <footer>
+        <H1>TRIU</H1>
+        <img src="../img/logo.png" alt="redes">
+    </footer>
+
+</body>
+
+</html>
