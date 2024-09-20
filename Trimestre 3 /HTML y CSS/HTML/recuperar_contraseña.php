21d53bd38f8549ce2314ce4602862e9313035f1c
Vale_parra <131938990+cars200@users.noreply.github.com>
2024-09-20 15:28:37 -0500

    Se añade estructura para recuperar_contraseña.php

---
+++ Trimestre 3 /HTML y CSS/HTML/recuperar_contraseña.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recupere su contraseña</title>
+    <link rel="stylesheet" href="../css/recuperar_contraseña.css">
+    <link rel="shortcut icon" href="../img/logo.ico" type="image/x-icon">
+</head>
+<body>
+    <div class="Cuadro">
+        <h1>Recuperar Contraseña</h1>
+        <p>Para poder recuperar su contraseña, y mantener toda su información de manera segura en Tria Optimize
+            solicitamos su correo electronico.
+        </p>
+        <input type="email" class="email" name=" id=" placeholder="Ingrese su correo electronico">
+        
+        <a href="../Php/inicio_sesion.html"><button>Volver al inicio de sesiòn</button></a>
+        <a href="#"><button>Enviar</button></a>
+        <img src="../img/logo.png" alt="">
+    </div>
+</body>
+</html>
