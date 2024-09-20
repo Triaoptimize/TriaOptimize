5ae1afb89e96a3e8ad6eec53f11efa6c12f9044e
Vale_parra <131938990+cars200@users.noreply.github.com>
2024-09-20 15:04:04 -0500

    Se añade estructura para inicio_cocinero.php

---
+++ Trimestre 3 /HTML y CSS/HTML/inicio_cocinero.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenid@ a tu sesión</title>
+    <link rel="stylesheet" href="../css/inicio_cocinero.css">
+    <link rel="shortcut icon" href="../img/logo.ico" type="image/x-icon">
+</head>
+<body>
+    
+    <header class="Arriba">
+        <img src="../img/Logo.png" alt="">
+        <h1>BIENVENID@</h1>
+        <img class="icon" src="../img/descarga.png" alt="">
+    </header>
+    <div class="seleccion">
+        <div  class="cuadro">
+            <img src="../img/menu.png" alt="" width="280px" height="250px">
+            <a href="pedidos_cocinero.php"><button>Historial de pedidos</button></a> 
+        </div>
+      
+      
+    </div>
+    <button class="cerrar"><a href="index.php">Cerar sesion</a></button>
+</body>
+</html>
