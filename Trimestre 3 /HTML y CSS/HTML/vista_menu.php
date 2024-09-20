404629ffa3b7c055d87132f0e6a882e7f0684757
Vale_parra <131938990+cars200@users.noreply.github.com>
2024-09-20 15:31:38 -0500

    Se añade estructura para vista_menu.php

---
+++ Trimestre 3 /HTML y CSS/HTML/vista_menu.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Triü Menu</title>
+    <link rel="stylesheet" href="../css/Vista_menu.css">
+</head>
+<body>
+    <div class="background-container">
+        <img class='mm' src="../triu.png" alt="">
+        <h1>Menú</h1>
+
+    <div class="container">
+        <div class="ll">
+            <h2>Quesos</h2>
+            <table class="comida">
+                <tr>
+                    <td>Queso 1</td>
+                    <td>1.000</td>
+                </tr>
+                <tr>
+                    <td>Queso 2</td>
+                    <td>1.000</td>
+                </tr>
+                <tr>
+                    <td>Queso 3</td>
+                    <td>1.000</td>
+                </tr>
+                <tr>
+                    <td>Queso 4</td>
+                    <td>1.000</td>
+                </tr>
+            </table>
+            <img class="pp" src="../img/comida.jpg" alt="">
+        </div>
+
+        <div class="ll">
+            <h2>Vinos</h2>
+            <table class="comida">
+                <tr>
+                    <td>Vino 1</td>
+                    <td>1.000</td>
+                </tr>
+                <tr>
+                    <td>Vino 2</td>
+                    <td>1.000</td>
+                </tr>
+                <tr>
+                    <td>Vino 3</td>
+                    <td>1.000</td>
+                </tr>
+                <tr>
+                    <td>Vino 4</td>
+                    <td>1.000</td>
+                </tr>
+            </table>
+            <img class='pp' src="../img/comida2.jpg" alt="Wine">
+        </div>
+
+        <div class="ll">
+            <h2>Sándwich</h2>
+            <table class="comida">
+                <tr>
+                    <td>Sándwich 1</td>
+                    <td>1.000</td>
+                </tr>
+                <tr>
+                    <td>Sándwich 2</td>
+                    <td>1.000</td>
+                </tr>
+                <tr>
+                    <td>Sándwich 3</td>
+                    <td>1.000</td>
+                </tr>
+                <tr>
+                    <td>Sándwich 4</td>
+                    <td>1.000</td>
+                </tr>
+            </table>
+            <img class='pp' src="../img/comida3.jpg" alt="Sandwich">
+        </div>
+    </div>
+
+
+        <button class="oo" id="change-background">Modo oscuro</button>
+    </div>
+
+    <script src="../java/menu.js"></script>
+</body>
+</html>
