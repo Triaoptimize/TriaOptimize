4ff4221b65a9d1d568a98b9e41998cef2f599dac
Vale_parra <131938990+cars200@users.noreply.github.com>
2024-09-20 15:12:35 -0500

    Se añade estructura para perfil_cocinero.php

---
+++ Trimestre 3 /HTML y CSS/HTML/perfil_cocinero.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido a tu perfil</title>
+    <link rel="stylesheet" href="../css/perfilcocinero.css">
+    <link rel="shortcut icon" href="/img/logo.ico" type="image/x-icon">
+</head>
+<body>
+    <div class="container">
+        <div class="profile-pic">
+            <img src="https://via.placeholder.com/100" alt="Foto de perfil">
+        </div>
+        <div class="info">
+            <h2>Cocinero</h2>
+            <p><strong>Nombre:</strong> Julián Jaldo Rivas</p>
+            <p><strong>Contraseña:</strong> 3135168%</p>
+            <p><strong>Teléfono:</strong> 31549865</p>
+            <p><strong>Correo:</strong> JulianRivas@gmail.com</p>
+        </div>
+        <div class="buttons">
+            <button>Regresar</button>
+            <button>Cambiar datos</button>
+        </div>
+    </div>
+</body>
+</html>
