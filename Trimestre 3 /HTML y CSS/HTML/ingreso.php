554dd8fa92eef69a118833742a7b38a0d4145e8d
Vale_parra <131938990+cars200@users.noreply.github.com>
2024-09-20 15:03:40 -0500

    Se añade estructura para ingreso.php

---
+++ Trimestre 3 /HTML y CSS/HTML/ingreso.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valida para ingresar</title>
+    <link rel="stylesheet" href="../css/ingreso.css">
+    <link rel="shortcut icon" href="../img/logo.ico" type="image/x-icon">
+</head>
+<body>
+    <div class="cuadro">
+        <h1>Ingrese su codigo de confirmación</h1>
+        <input type="text" maxlength="1" class="input-box">
+        <input type="text" maxlength="1" class="input-box">
+        <input type="text" maxlength="1" class="input-box">
+        <input type="text" maxlength="1" class="input-box">
+        <input type="text" maxlength="1" class="input-box">
+        <input type="text" maxlength="1" class="input-box">
+        <br>
+        <br>
+        <a href="index.php">Volver</a>
+        <a href="inicio_sesion.php">Validar</a>
+    </div>
+   
+</body>
+</html>
