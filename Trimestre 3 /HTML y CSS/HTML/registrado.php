5d621bfff0d1d0e2512eacc760093ffca655eb19
Vale_parra <131938990+cars200@users.noreply.github.com>
2024-09-20 15:29:09 -0500

    Se añade estructura para registrado.php

---
+++ Trimestre 3 /HTML y CSS/HTML/registrado.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido empleado de TRIU</title>
+    <link rel="stylesheet" href="../css/registrado.css">
+    <link rel="shortcut icon" href="../img/logo.ico" type="image/x-icon">
+</head>
+<body>
+    <div class="cuadro">
+        <img src="../img/logo.png" alt="">
+        <h1>BIENVENIDO</h1>
+        <p>Nos complace tenerte con nosotros. Tu registro en nuestra plataforma es el primer paso para conectarte con todo lo que necesitas para tener éxito en tu nueva posición.
+
+            En nuestro sitio web, encontrarás acceso a recursos clave, información importante y herramientas que te ayudarán a desempeñar tus tareas de manera eficiente. Si necesitas ayuda en cualquier momento, no dudes en ponerte en contacto con nosotros.
+            
+            Estamos encantados de tenerte como parte del equipo y esperamos que esta nueva etapa sea emocionante y productiva.</p>
+            <button><a href="inicio_sesion.php">Volver al inicio de sesión</button></a>
+    </div>
+</body>
+</html>
