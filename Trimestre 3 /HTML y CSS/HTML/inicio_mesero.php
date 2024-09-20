283a8822ca35f10fdcbb62b1b12d670cc64cf7fb
Vale_parra <131938990+cars200@users.noreply.github.com>
2024-09-20 15:05:59 -0500

    Se añade estructura para inicio_mesero.php

---
+++ Trimestre 3 /HTML y CSS/HTML/inicio_mesero.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenid@ a tu sesión</title>
+    <link rel="stylesheet" href="../css/inicio_cocinero.css">
+    <link rel="shortcut icon" href="/img/logo.ico" type="image/x-icon">
+</head>
+<body>
+    
+    <header class="Arriba">
+        <img src="../img/Logo.png" alt="">
+        <h1>BIENVENID@</h1>
+        <img class="icon" src="../img/descarga.png" alt="">
+    </header>
+    <div class="seleccion">
+        <div  class="cuadro">
+            <img src="../img/menu.png" alt="" width="250px" height="250px">
+            <a href="Vista_menu.php"><button>menú</button></a>
+        </div>
+        <div class="cuadro">
+            <img src="../img/pedido.png" alt="" width="250px" height="250px">
+            <a href="Pedidos.php"><button> pedido</button></a> 
+        </div>
+        <div class="cuadro">
+            <img src="../img/factura.png" alt=""width="250px" height="250px">
+            <a href="factura.php"><button>factura</button></a>
+        </div>
+      
+    </div>
+    <button class="cerrar"><a href="index.php">Cerar sesion</a></button>
+</body>
+</html>
