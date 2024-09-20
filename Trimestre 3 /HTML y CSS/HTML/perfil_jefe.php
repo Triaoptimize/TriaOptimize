b23a1b2f90a857a8929d5b1bf80863068937c914
Vale_parra <131938990+cars200@users.noreply.github.com>
2024-09-20 15:18:11 -0500

    Se añade la eestructura para  perfil_jefe.php

---
+++ Trimestre 3 /HTML y CSS/HTML/perfil_jefe.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido a tu perfil</title>
+    <link rel="stylesheet" href="../css/perfil_jefe.css">
+    <link rel="shortcut icon" href="../img/logo.ico" type="image/x-icon">
+</head>
+<body>
+    <div class="container">
+        <div class="profile-pic">
+            <img src="https://via.placeholder.com/100" alt="Foto de perfil">
+        </div>
+        <div class="info">
+            <h2>Jefe</h2>
+            <p><strong>Nombre:</strong> Julián Jaldo Rivas</p>
+            <p><strong>Contraseña:</strong> 3135168%</p>
+            <p><strong>Teléfono:</strong> 31549865</p>
+            <p><strong>Correo:</strong> JulianRivas@gmail.com</p>
+        </div>
+        <div class="buttons">
+         <a href="../Php/inicio_jefe.php">  <button>Regresar</button></a> 
+            <button src="">Cambiar datos</button>
+        </div>
+    </div> 
+</body>
+</html>
